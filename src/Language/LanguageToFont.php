fff75e4127d0f5aea0d9fcd35c35a2515ba15701
Jake Jackson <jake@gravitypdf.com>
2025-12-24 10:28:31 +1100

    Continue refactoring

--- src/Language/LanguageToFont.php
+++ src/Language/LanguageToFont.php
@@ -106,45 +106,188 @@ class LanguageToFont implements \Mpdf\Language\LanguageToFontInterface
 				$coreSuitable = true;
 				break;
 
-			//CASE 'bax':	// BAMUM
-			//CASE 'ha':  CASE 'hau':	// Hausa
+			case 'ru':
+			case 'rus': // Russian	// CYRILLIC
+			case 'ab':
+			case 'abk': // Abkhaz
+			case 'av':
+			case 'ava': // Avaric
+			case 'ba':
+			case 'bak': // Bashkir
+			case 'be':
+			case 'bel': // Belarusian
+			case 'bg':
+			case 'bul': // Bulgarian
+			case 'ce':
+			case 'che': // Chechen
+			case 'cv':
+			case 'chv': // Chuvash
+			case 'kk':
+			case 'kaz': // Kazakh
+			case 'kv':
+			case 'kom': // Komi
+			case 'ky':
+			case 'kir': // Kyrgyz
+			case 'mk':
+			case 'mkd': // Macedonian
+			case 'cu':
+			case 'chu': // Old Church Slavonic
+			case 'os':
+			case 'oss': // Ossetian
+			case 'sr':
+			case 'srp': // Serbian
+			case 'tg':
+			case 'tgk': // Tajik
+			case 'tt':
+			case 'tat': // Tatar
+			case 'tk':
+			case 'tuk': // Turkmen
+			case 'uk':
+			case 'ukr': // Ukrainian
+			case 'hy':
+			case 'hye': // ARMENIAN
+			case 'ka':
+			case 'kat': // GEORGIAN
+			case 'el':
+			case 'ell': // GREEK
+			case 'cop': // COPTIC
+			case 'got': // GOTHIC
+
+			/* African */
+			case 'nqo': // NKO
+			case 'bax':	// BAMUM
+			case 'ha':
+			case 'hau':	// Hausa
+			case 'vai': // VAI
+			case 'am':
+			case 'amh': // Amharic ETHIOPIC
+			case 'ti':
+			case 'tir': // Tigrinya ETHIOPIC
 
 			/* Middle Eastern */
-
-
-			//CASE 'arc':	// IMPERIAL_ARAMAIC
-			//CASE ''ae:	// AVESTAN
-			//CASE 'peo':	// OLD_PERSIAN
-			//CASE 'mid':	// MANDAIC
-			//CASE 'smp':	// SAMARITAN
+			case 'ar':
+			case 'ara': // Arabic	NB Arabic text identified by Autofont will be marked as und-Arab
+			case 'fa':
+			case 'fas': // Persian (Farsi)
+			case 'ps':
+			case 'pus': // Pashto
+			case 'ku':
+			case 'kur': // Kurdish
+			case 'ur':
+			case 'urd': // Urdu
+			case 'he':
+			case 'heb': // HEBREW
+			case 'yi':
+			case 'yid': // Yiddish
+			case 'syr': // SYRIAC
+			case 'arc':	// IMPERIAL_ARAMAIC
+			case 'ae':	// AVESTAN
+			case 'xcr': // CARIAN
+			case 'xlc': // LYCIAN
+			case 'xld': // LYDIAN
+			case 'mid':	// MANDAIC
+			case 'peo':	// OLD_PERSIAN
+			case 'phn': // PHOENICIAN
+			case 'smp':	// SAMARITAN
+			case 'uga': // UGARITIC
 
 			/* Central Asian */
-
-			//CASE 'mn':  CASE 'mon':	// MONGOLIAN	(Vertical script)
-			//CASE 'ug':  CASE 'uig':	// Uyghur
-			//CASE 'uz':  CASE 'uzb':	// Uzbek
-			//CASE 'az':  CASE 'azb':	// South Azerbaijani
+			case 'bo':
+			case 'bod': // TIBETAN
+			case 'dz':
+			case 'dzo': // Dzongkha
+			case 'mn':
+			case 'mon':	// MONGOLIAN	(Vertical script)
+			case 'ug':
+			case 'uig':	// Uyghur
+			case 'uz':
+			case 'uzb':	// Uzbek
+			case 'az':
+			case 'azb':	// South Azerbaijani
 
 			/* South Asian */
+			case 'as':
+			case 'asm': // Assamese
+			case 'bn':
+			case 'ben': // BENGALI; Bangla
+			case 'ks':
+			case 'kas': // Kashmiri
+			case 'hi':
+			case 'hin': // Hindi	DEVANAGARI
+			case 'bh':
+			case 'bih': // Bihari (Bhojpuri, Magahi, and Maithili)
+			case 'sa':
+			case 'san': // Sanskrit
+			case 'gu':
+			case 'guj': // Gujarati
+			case 'pa':
+			case 'pan': // Panjabi, Punjabi GURMUKHI
+			case 'kn':
+			case 'kan': // Kannada
+			case 'mr':
+			case 'mar': // Marathi
+			case 'ml':
+			case 'mal': // MALAYALAM
+			case 'ne':
+			case 'nep': // Nepali
+			case 'or':
+			case 'ori': // ORIYA
+			case 'si':
+			case 'sin': // SINHALA
+			case 'ta':
+			case 'tam': // TAMIL
+			case 'te':
+			case 'tel': // TELUGU
+				// Do nothing
+				break;
 
+			// Sindhi (Arabic or Devanagari)
+			case 'sd':
+			case 'snd': // Sindhi
+				if ($country === 'in') {
+					$unifont = 'freeserif';
+				}
+				break;
 
-			//CASE 'ccp':	// CHAKMA
-			//CASE 'lep':	// LEPCHA
-
-			//CASE 'sat':	// OL_CHIKI
-			//CASE 'saz':	// SAURASHTRA
-
-			//CASE 'dgo':	// TAKRI
+			case 'ccp':	// CHAKMA
+			case 'lep':	// LEPCHA
+			case 'lif': // LIMBU
+			case 'sat':	// OL_CHIKI
+			case 'saz':	// SAURASHTRA
+			case 'syl': // SYLOTI_NAGRI
+			case 'dgo':	// TAKRI
+			case 'dv':
+			case 'div': // Divehi; Maldivian  THAANA
 
 			/* South East Asian */
+			case 'km':
+			case 'khm': // KHMER
+			case 'lo':
+			case 'lao': // LAO
+			case 'my':
+			case 'mya': // MYANMAR Burmese
+			case 'th':
+			case 'tha': // THAI
+			case 'vi':
+			case 'vie': // Vietnamese
+			case 'ms':
+			case 'msa':	// Malay
+			case 'ban':	// BALINESE
+			case 'bya':	// BATAK
+			case 'bug': // BUGINESE
+			case 'cjm':	// CHAM
+			case 'jv':	// JAVANESE
+			case 'su': // SUNDANESE
+			case 'tdd': // TAI_LE
+			case 'blt': // TAI_VIET
 
-			//CASE 'ms':  CASE 'msa':	// Malay
-			//CASE 'ban':	// BALINESE
-			//CASE 'bya':	// BATAK
-			//CASE 'cjm':	// CHAM
-			//CASE 'jv':	// JAVANESE
-
-
+			/* Phillipine */
+			case 'bku': // BUHID
+			case 'hnn': // HANUNOO
+			case 'tl': // TAGALOG
+			case 'tbw': // TAGBANWA
+				// Do nothing
+				break;
 
 			/* East Asian */
 			case 'zh':
@@ -178,6 +321,19 @@ class LanguageToFont implements \Mpdf\Language\LanguageToFontInterface
 				}
 				break;
 
+			case 'lis': // LISU
+
+			/* American */
+			case 'chr': // CHEROKEE
+			case 'oj':
+			case 'oji': // Ojibwe; Chippewa
+			case 'cr':
+			case 'cre': // Cree CANADIAN_ABORIGINAL
+			case 'iu':
+			case 'iku': // Inuktitut
+				// Do nothing
+				break;
+
 			/* Undetermined language - script used */
 			case 'und':
 				$unifont = $this->fontByScript($script, $adobeCJK);
@@ -191,45 +347,70 @@ class LanguageToFont implements \Mpdf\Language\LanguageToFontInterface
 	{
 		switch ($script) {
 			/* European */
-
+			case 'latn': // LATIN
+			case 'cyrl': // CYRILLIC
+			case 'cprt': // CYPRIOT
+			case 'glag': // GLAGOLITIC;
+			case 'linb': // LINEAR_B
+			case 'ogam': // OGHAM
+			case 'ital': // OLD_ITALIC
+			case 'runr': // RUNIC
+			case 'shaw': // SHAVIAN
 
 			/* African */
-			//CASE 'merc':	// MEROITIC_CURSIVE
-			//CASE 'mero':	// MEROITIC_HIEROGLYPHS
-
+			case 'egyp': // EGYPTIAN_HIEROGLYPHS
+			case 'ethi': // ETHIOPIC
+			case 'merc': // MEROITIC_CURSIVE
+			case 'mero': // MEROITIC_HIEROGLYPHS
+			case 'osma': // OSMANYA
+			case 'tfng': // TIFINAGH
 
 			/* Middle Eastern */
-			//CASE 'sarb':	// OLD_SOUTH_ARABIAN
-			//CASE 'prti':	// INSCRIPTIONAL_PARTHIAN
-			//CASE 'phli':	// INSCRIPTIONAL_PAHLAVI
-
+			case 'arab': // ARABIC
+			case 'xsux': // CUNEIFORM
+			case 'sarb': // OLD_SOUTH_ARABIAN
+			case 'prti': // INSCRIPTIONAL_PARTHIAN
+			case 'phli': // INSCRIPTIONAL_PAHLAVI
 
 			/* Central Asian */
-			//CASE 'orkh':	// OLD_TURKIC
-			//CASE 'phag':	// PHAGS_PA		(Vertical script)
+			case 'orkh': // OLD_TURKIC
+			case 'phag': // PHAGS_PA (Vertical script)
 
 			/* South Asian */
-			//CASE 'brah':	// BRAHMI
-			//CASE 'kthi':	// KAITHI
-			//CASE 'shrd':	// SHARADA
-			//CASE 'sora':	// SORA_SOMPENG
+			case 'brah': // BRAHMI
+			case 'kthi': // KAITHI
+			case 'khar': // KHAROSHTHI
+			case 'mtei': // MEETEI_MAYEK
+			case 'shrd': // SHARADA
+			case 'sora': // SORA_SOMPENG
 
 			/* South East Asian */
-			//CASE 'rjng':	// REJANG
+			case 'kali': // KAYAH_LI
+			case 'rjng': // REJANG
+			case 'lana': // TAI_THAM
+			case 'talu': // NEW_TAI_LUE
+				// Do nothing
+				break;
 
 			/* East Asian */
 			case 'hans': // HAN (SIMPLIFIED)
 				if ($adobeCJK) {
 					return 'gb';
 				}
+				break;
 
-			//CASE 'plrd':	// MIAO
+			case 'bopo': // BOPOMOFO
+			case 'plrd': // MIAO
+			case 'yiii': // YI
 
 			/* American */
+			case 'dsrt': // DESERET
 
+			/* Other */
+			case 'brai': // BRAILLE
 		}
 
-		return null;
+		return '';
 	}
 
 }
