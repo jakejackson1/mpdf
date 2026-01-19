3eacc3add5208be5e0ccba59bda34a821aa19590
Jake Jackson <jake@gravitypdf.com>
2026-01-19 17:02:42 +1100

    Remove Fpdi dependency from BaseWriter

--- src/Writer/MetadataWriter.php
+++ src/Writer/MetadataWriter.php
@@ -569,7 +569,7 @@ class MetadataWriter implements \Psr\Log\LoggerAwareInterface
 
 								foreach ($values as $name => $entry) {
 									$this->writer->write('/' . $name . ' ', false);
-									$this->writer->writePdfType($entry);
+									$this->mpdf->writePdfType($entry);
 								}
 
 								if (isset($pl['quadPoints'])) {
